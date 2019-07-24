431fc1ae5875f6f42f3b4ba64c677c482e46149f
Martin Helmut Dominguez Alvarez <martinhelmutmh@gmail.com>
2019-07-23 21:10:54 -0500

    Add

---
+++ score-keep/client/main.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Meteor} from 'meteor/meteor';
+import {Tracker} from 'meteor/tracker';
+
+//MiniMongoDB
+import {Players} from './../imports/api/players.js';
+
+const renderPlayers= (playerList) => {
+  return playerList.map((player) => (
+    <p key={player._id}>
+      {'¡'+player.name } has {player.score} point(s) !
+      <button onClick={()=> Players.update({_id:player._id},{$inc:{score:1}})}>+1</button>
+      <button onClick={()=> Players.update({_id:player._id},{$inc:{score:-1}})}>-1</button>
+      <button onClick={()=> Players.remove({_id:player._id})}>X</button>
+    </p>
+  ));
+};
+
+const handleSubmit = (event) => {
+  let playerName = event.target.playerName.value;
+  //Prevent refresh
+  event.preventDefault();
+  if (playerName){
+    event.target.playerName.value='';
+    Players.insert({
+      name : playerName,
+      score : 0
+    })
+  }
+}
+
+Meteor.startup(() => {
+  Tracker.autorun(() => {
+    let players= Players.find().fetch();
+    let title = 'Score Keep';
+    let name = 'Martin Helmut';
+    let jsx = (
+      <div>
+        <h1>{title}</h1>
+        <p>Hello {name}!</p>
+        <p>Esta es la prueba</p>
+        {renderPlayers(players)}
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="playerName" placeholder="Nombre del jugador"></input>
+          <button>Agregar jugador</button>
+        </form>
+      </div>
+    );
+    ReactDOM.render(jsx, document.getElementById('app'));
+  });
+});
